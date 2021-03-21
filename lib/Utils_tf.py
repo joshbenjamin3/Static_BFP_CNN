d084e1b45eaf358d575a2cde87a0aa77f469f069
Grace Tian <gracetian6@gmail.com>
2021-03-20 23:20:09 -0400

    kp exp grace's

--- lib/Utils_tf.py
+++ lib/Utils_tf.py
@@ -88,9 +88,110 @@ def smooth_hist(array, eps=0.0001):
     n_nonzeros = tf.size(array) - n_zeros
     if (n_nonzeros.item() == 0):
         raise ValueError("All the values are zeros, the array shape is:", array)
-    eps1 = eps * tf.to_float(n_zeros).item() / tf.to_float(n_nonzero).item()
+    eps1 = eps * tf.reduce_sum(tf.to_float(n_zeros)) / tf.reduce_sum(tf.to_float(n_nonzero)) # tf.item
     #print("eps1:", eps1)
     array = tf.to_float(array)
     array += eps * is_zeros + (-eps1) * is_nonzeros
     assert tf.reduce_sum(array <= 0) == 0, "Some negtive values are generated during smoothing the histogram"
     return array
+
+def find_exp_KL_act(array, MANTISSA_WIDTH, EXPONENT_WIDTH, group=1, eps=0.0001, bins_factor=3):
+    # Find the proper exponent value instead of max_exp by minimize the KL_divergence
+    #   num_bins is used to construct the histogram/distribution
+    #   eps is used to smooth the histogram/distribution
+    # Assuming the input has shape [batch, channel, height, width]
+
+    # Reshape to [batch, channel, height*width]
+    array = array.cuda()
+    orig_shape = tf.shape(array)
+    group = orig_shape[1] if (group>orig_shape[1]) else group # group is whole channel when group is -1
+    number_of_blocks = math.ceil(orig_shape[1]/group)
+    opt_exp = tf.zeros((1)) #torch.empty
+    max_exp = tf.zeros((1))
+    if orig_shape[1] % group == 0:
+        # Find the max_exp
+        array = torch.reshape(array, (orig_shape[0], number_of_blocks, group*orig_shape[2]))
+        exp_array = find_exponent(array, EXPONENT_WIDTH)
+        max_exp = find_max_exponent(exp_array, quant_dim=len(array.shape)-1)
+        max_exp = find_max_exponent(max_exp, quant_dim=0)
+        opt_exp = tf.identity(max_exp)
+        # Unsqeeze for quantization use
+        us_max_exp = max_exp.unsqueeze(0)
+        # Compute the histogram of original internal features
+        orig_hist = []
+        orig_max = []
+        orig_min = []
+        orig_num_bins = []
+        min_kl_div = []
+        #print("bins factor:", bins_factor)
+        for i in range(number_of_blocks):
+            flat_array = torch.flatten(array[:, i, :].cpu())
+            float_max = tf.reduce_max(flat_array, 0)[0].item()
+            float_min = tf.reduce_min(flat_array, 0)[0].item()
+            #print ("orignal max", float_max, "original min", float_min)
+            target_max_int = (int)(math.ceil(float_max))
+            target_min_int = (int)(math.floor(float_min))
+            # For mobilenet only
+            target_diff = target_max_int - target_min_int
+            if (target_diff < 6):
+                interval = 0.02
+                #num_bins = 1 + (int)((target_max_int - target_min_int)/interval) #8-2 for resnet, 8-5 for inceptionv4 8-5 for vgg16
+            else:
+                #interval = (float_max - float_min)/ 128
+                interval = 0.035
+                #num_bins = 1 + (int)((target_max_int - target_min_int)/(2/(2**(MANTISSA_WIDTH-bins_factor)))) #8-2 for resnet, 8-5 for inceptionv4 8-5 for vgg16
+            #num_bins = 1 + (int)((target_max_int - target_min_int)/interval) #8-2 for resnet, 8-5 for inceptionv4 8-5 for vgg16
+            #num_bins = 1 + (int)((target_max_int - target_min_int)/(2/(2**(MANTISSA_WIDTH-bins_factor)))) #8-2 for resnet, 8-5 for inceptionv4 8-5 for vgg16
+
+            float_board = abs(float_max) if (abs(float_max) > abs(float_min)) else abs(float_min)
+            #float_interval = (2*float_board)/64 # Indicate how accurate the distribution needs
+
+            #float_interval = (2*float_board)/64 if (float_min<0) else (float_board)/64
+
+            float_interval = (float_max-float_min)/128 # Indicate how accurate the distribution needs 70.62-128
+            num_bins = 1 + (int)((target_max_int - target_min_int)/float_interval)
+
+            #num_bins = 1 + (int)((target_max_int - target_min_int)/(2/(2**(MANTISSA_WIDTH-bins_factor)))) #8-2 for resnet, 8-5 for inceptionv4 8-5 for vgg16
+            target_hist = torch.histc(flat_array, bins=num_bins, min=target_min_int, max=target_max_int)
+            #print ("flat array", flat_array.shape)
+            # Smoth the target histogram
+            target_hist = smooth_hist(target_hist, eps)
+            # Nomalize the target histogram
+            target_hist = target_hist/target_hist.sum()
+            # Add information into list
+            orig_hist.append(target_hist)
+            orig_max.append(target_max_int)
+            orig_min.append(target_min_int)
+            orig_num_bins.append(num_bins)
+            min_kl_div.append(sys.float_info.max)
+
+        # Quantize accodingly, Here we only explore (max_exp-6) ~ max_exp
+        for i in range(3):
+            quantized_array = to_exponent_mantissa_width(array, us_max_exp-i, MANTISSA_WIDTH,
+                                                        quant_dim=len(array.shape)-1)
+            for j in range(number_of_blocks):
+                flat_qarray = torch.flatten(quantized_array[:, j, :].cpu())
+                if (((torch.max(flat_qarray, 0))[0].item() < orig_min[j])):
+                    continue
+                quantized_hist = torch.histc(flat_qarray, bins=orig_num_bins[j],
+                                            min=orig_min[j], max=orig_max[j])
+                # Smoth the quantized histogram
+                quantized_hist = smooth_hist(quantized_hist, eps)
+                # Log-Nomalize the quantized histogram
+                quantized_hist = quantized_hist/quantized_hist.sum()
+                quantized_hist = torch.log(quantized_hist)
+                # Calculate the KL-Divergence
+                kl_div = F.kl_div(quantized_hist, orig_hist[j])
+                if (min_kl_div[j] > kl_div.item()):
+                    opt_exp[j] = (max_exp[j]-i)
+                    min_kl_div[j] = kl_div.item()
+    else:
+        raise ValueError("Channel is not divisible by group  while determining the opt exponent list the separated activation")
+    num_nequal = (max_exp != opt_exp).sum()
+    logging.debug("After minimizing the KL divergence, %d / %d shared act exponents are improved" % (num_nequal.item(), opt_exp.numel()))
+    opt_exp = opt_exp.int().cpu().data.tolist()
+    opt_exp = np.repeat(opt_exp, group)
+    max_exp = max_exp.int().cpu().data.tolist()
+    max_exp = np.repeat(max_exp, group)
+    #print ("kl div:", min_kl_div)
+    return opt_exp, max_exp
